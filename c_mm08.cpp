a884af7c57f95a08cd09fcde4c054cbfc21edac9
bob <40296033+lgl107u@users.noreply.github.com>
2020-06-28 08:44:52 +0800

    Create c_mm08.cpp

---
+++ c_mm08.cpp
@@ -0,0 +1,15 @@
+#include <iostream>  
+#include <iomanip>    
+  
+using namespace std;    
+    
+int main()      
+{      
+    int x,y;  
+    while(cin >> x >> y)  
+    {  
+        int sum = x+y;  
+        cout << sum*sum << endl;  
+    }  
+    return 0;      
+}  
