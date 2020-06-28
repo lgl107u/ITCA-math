a0803e4dc96a6e8016e3c879eb9ac690cf886003
bob <40296033+lgl107u@users.noreply.github.com>
2020-06-28 08:43:08 +0800

    Create c_mm03.cpp

---
+++ c_mm03.cpp
@@ -0,0 +1,13 @@
+#include <iostream>  
+#include <iomanip>  
+using namespace std;  
+  
+int main()    
+{    
+    int x,y,z;    
+    while(cin >> x >> y)   
+    {  
+        cout << x+y << endl;   
+    }  
+    return 0;    
+}  
