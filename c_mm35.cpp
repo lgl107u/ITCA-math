ba0210348a4a9a10366f6fab210fffab292cf6ed
bob <40296033+lgl107u@users.noreply.github.com>
2020-07-01 11:44:07 +0800

    Update c_mm35.cpp

--- c_mm35.cpp
+++ c_mm35.cpp
@@ -5,10 +5,10 @@
 int main(void)    
 {    
     int input;    
-    while(scanf("%d",&input)!=EOF)    
+    while(cin >> input)    
     {    
-       if( (input%4==0 && input%100!=0) || input%400==0) printf("Bissextile Year\n");  
-       else printf("Common Year\n");  
+       if( (input%4==0 && input%100!=0) || input%400==0) cout << "Bissextile Year\n";  
+       else cout << "Common Year\n";  
     }    
     return 0;    
 }
