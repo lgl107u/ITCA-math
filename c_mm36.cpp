5a69c6c5a320921ba806d1106b5dde8fe23c22d8
bob <40296033+lgl107u@users.noreply.github.com>
2020-07-01 11:41:49 +0800

    Update c_mm36.cpp

--- c_mm36.cpp
+++ c_mm36.cpp
@@ -5,12 +5,12 @@
 int main(void)    
 {    
     int input;    
-    while(scanf("%d",&input)!=EOF)    
+    while(cin >> input )    
     {    
-        if ( input>=3 && input<=5 ) printf("Spring\n");  
-        else if ( input>=6 && input<=8 ) printf("Summer\n");  
-        else if ( input>=9 && input<=11 ) printf("Autumn\n");  
-        else if ( input=12 || (input<=2 && input>=1) ) printf("Winter\n");  
+        if ( input>=3 && input<=5 ) cout <<"Spring\n";  
+        else if ( input>=6 && input<=8 ) cout << "Summer\n";  
+        else if ( input>=9 && input<=11 ) cout <<"Autumn\n";  
+        else if ( input=12 || (input<=2 && input>=1) ) cout << "Winter\n";  
     }    
     return 0;    
 }
